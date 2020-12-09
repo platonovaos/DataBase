09d377004a21f4ea5369556e91e009a0f22eaf29
platonovaos <66078263+platonovaos@users.noreply.github.com>
2020-12-09 14:51:30 +0300

    add main sql

---
+++ lab5/jsonWork.sql
@@ -0,0 +1,35 @@
+﻿--Из таблиц базы данных, созданной в первой лабораторной работе, 
+--извлечь данные в JSON
+SELECT to_jsonb(Tour) FROM Tour;
+SELECT to_jsonb(Food) FROM Food;
+SELECT to_jsonb(Hotel) FROM Hotel;
+SELECT to_jsonb(City) FROM City;
+
+
+--Выполнить загрузку и сохранение JSON файла в таблицу.
+--Создание
+COPY (SELECT to_jsonb(Tour) FROM Tour) 
+TO '/home/main/Desktop/BMSTU/5sem/DataBase/lab5/tour.json';
+
+--Загрузка
+CREATE TEMP TABLE IF NOT EXISTS tourJSONB(Tour jsonb);
+COPY tourJSONB 
+FROM '/home/main/Desktop/BMSTU/5sem/DataBase/lab5/tour.json';
+
+SELECT *
+FROM tourJSONB;
+
+/*CREATE TEMP TABLE IF NOT EXISTS tourTEMP
+(
+	TourID INT,
+	Food INT,
+	Hotel INT,
+	Cost INT,
+	DateBegin DATE,
+	DateEnd DATE
+);
+*/
+
+SELECT * 
+FROM tourJSONB, jsonb_populate_record(null::tour, tour);
+
